a5f1b6773a1e12916ae02d775f7dd4d90b5a0880
Mhmmtak <akmuhammett3483@gmail.com>
2025-06-17 10:45:21 -0500

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -2,7 +2,7 @@ package day01;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("Hello Universe");
         System.out.println("Hello World");
-        System.out.println("Hello ");
     }
 }
