f76fcdd482a07652b50c0955edf3c131ee7d136b
muhammetakdeniz <akmuhammett3483@gmail.com>
2025-06-17 10:55:00 -0500

    Revert "Update HelloCydeo.java"
    This reverts commit 9ce9a6c45c6932063e67e0a4a156256827ef0607.

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,9 +3,5 @@ package day01;
 public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
-   
-        System.out.println("Hello Universe");
-     
-
     }
 }
